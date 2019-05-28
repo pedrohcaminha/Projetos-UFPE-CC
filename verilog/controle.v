579dab02cb0e64cd441e8f74b31c8483f19f0fee
pedrohcaminha <pedrorecife@gmail.com>
2019-05-28 12:58:54 -0300

    auto Ter 28 Mai 2019 12:58:54 -03

---
+++ verilog/controle.v
@@ -0,0 +1,62 @@
+module controle(sel, auxX, auxY, auxZ, auxULA);
+
+input 3:0] sel;
+output reg [1:0] auxX;
+output reg [2:0] auxY;
+output reg [1:0] auxZ;
+output reg auxULA;
+
+parameter LIMPARXZ = 2'b00;
+parameter CARREGARXZ = 2'b01;
+parameter MANTERXZ = 2'b10;
+
+parameter LIMPARY = 3'b000;
+parameter CARREGARY = 3'b001;
+parameter MANTERY = 3'b010;
+parameter SESQUERDAY = 3'b011;
+parameter SDIREITAY = 3'b100;
+
+parameter SOMAULA = 1'b0;
+parameter SUBULA = 1'b1;
+
+always begin
+	case(func)
+		4'b0000:
+			begin
+				auxX <= CARREGARXZ;
+				auxY <= LIMPARY;
+				auxZ <= LIMPARXZ;
+				auxULA <= SOMAULA;
+			end
+		4'b0001:
+			begin
+				auxX <= CARREGARXZ;
+				auxY <= CARREGARY;
+				auxZ <= MANTERXZ;
+				auxULA <= SOMAULA;
+			end
+		4'b0010:
+			begin
+				auxX <= MANTERXZ;
+				auxY <= CARREGARY;
+				auxZ <= MANTERXZ;
+				auxULA <= SOMAULA;
+			end
+		4'b0011:
+			begin
+				auxX <= MANTERXZ;
+				auxY <= SDIREITAY;
+				auxZ <= MANTERXZ;
+				auxULA <= SOMAULA;
+			end
+		4'b0100:
+			begin
+				auxX <= LIMPARXZ;
+				auxY <= LIMPARY;
+				auxZ <= CARREGARXZ;
+				auxULA <= SOMAULA;
+			end
+	endcase
+end
+
+endmodule
---
+++ verilog/soma.v
@@ -0,0 +1,15 @@
+module soma(sel, X, Y, saida);
+    input [3:0] X;
+    input [3:0] Y;
+    output reg saida [3:0];
+    input sel;
+
+    always begin
+        if(sel == 0) //soma
+            saida <= X + Y;
+        else if(sel == 1)begin //sub
+            saida <= X - Y;
+        end
+    end
+
+endmodule // soma e subtracao com vetor de selecao para 0 sendo soma e 1 sendo subtracao
